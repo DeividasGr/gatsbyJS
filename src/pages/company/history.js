584216f554475020d00e76340636dc4df67b84be
Gruzinas1 <dgruzdzevicius@gmail.com>
2021-03-31 11:21:18 +0300

    linking pages

--- src/pages/company/history.js
+++ src/pages/company/history.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 
 function history() {
-  return <div>history of our company</div>;
+  return (
+    <div>
+      history of our company
+      <Link to="/">Home</Link>
+    </div>
+  );
 }
 
 export default history;
