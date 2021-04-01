c04ffeab118ff05dc8ba079308be82485b2ed40f
Gruzinas1 <dgruzdzevicius@gmail.com>
2021-04-01 09:25:43 +0300

    footer component setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built with
+        &nbsp;
+        <a href="https://www.gatsbyjs.com/">Gatbsy</a>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
